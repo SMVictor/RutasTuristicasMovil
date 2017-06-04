2d78681a173b986fd38d7f31dcc79e68bbae5f21
SMVictor <victor.salvatierra@outlook.com>
2017-06-04 17:00:56 -0600

    Inicio de sesión y principal

--- app/src/main/java/com/example/victo/rutasturisticas/LoginFragment.java
+++ app/src/main/java/com/example/victo/rutasturisticas/LoginFragment.java
@@ -1,29 +1,40 @@
 package com.example.victo.rutasturisticas;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.example.victo.rutasturisticas.Modules.VolleyS;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.LinkedList;
 
 public class LoginFragment extends Fragment {
 
-    private TextView label;
-    private VolleyS volley;
-    protected RequestQueue fRequestQueue;
     private View view;
-    private Button btnSeleccionarRutas;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
-    }
 
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_login, container, false);
+        return  view;
+    }
 }
